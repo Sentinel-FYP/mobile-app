efc7516befd55121f87b618dcdb04c789a84ae6e
Ibrahim <mibrahim37612@gmail.com>
2023-11-19 01:24:15 +0500

    [Add] Login signup

--- src/screens/auth/LoginScreen.jsx
+++ src/screens/auth/LoginScreen.jsx
@@ -1,30 +1,51 @@
-import { View, StyleSheet } from 'react-native'
-import React from 'react'
-import { Text } from '@rneui/base'
-import { Button, Input } from '@rneui/themed'
-import { COLORS } from '../../constants'
+import { View, StyleSheet, ScrollView } from "react-native";
+import React from "react";
+import { Button, Input, Text } from "@rneui/themed";
+import { COLORS } from "../../constants";
 
-
-const Login = ({navigation}) => {
-  const handleRegisterNowPress = ()=>{
-    navigation.navigate("Register")
-  }
+const Login = ({ navigation }) => {
+  const handleRegisterNowPress = () => {
+    navigation.navigate("Register");
+  };
   return (
-    <View style={{flex:1,justifyContent: 'center', alignItems: 'center', paddingHorizontal: 30}}>
-      <Text h2>Login</Text>
-      <View style={{width: '100%', alignItems: 'center', paddingVertical:40}}>
-      <Input label={"Email"} labelStyle={styles.inputLabel} />
-      <Input label={"Password"} labelStyle={styles.inputLabel}/>
-      <Button color='primary' title={'Login'} size='lg' containerStyle={{width: 200, borderRadius:5,}}/>
+    <ScrollView
+      style={{ width: "100%" }}
+      contentContainerStyle={{
+        flexGrow: 1,
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingHorizontal: 30,
+      }}
+    >
+      <Text h2>Register</Text>
+      <View
+        style={{ width: "100%", alignItems: "center", paddingVertical: 40 }}
+      >
+        <Input label={"Full Name"} labelStyle={styles.inputLabel} />
+        <Input label={"Email"} labelStyle={styles.inputLabel} />
+        <Input label={"Password"} labelStyle={styles.inputLabel} />
+        <Input label={"Confirm Password"} labelStyle={styles.inputLabel} />
+        <Button
+          color="primary"
+          title={"Register"}
+          size="lg"
+          containerStyle={{ width: 200, borderRadius: 5 }}
+        />
       </View>
-      <Text style={{paddingVertical: 10}}>Dont have an account? <Text style={{color: 'blue', fontWeight: 'bold'}} onPress={handleRegisterNowPress}>Register Now</Text></Text>
-    </View>
-  )
-}
+      <Text style={{ paddingVertical: 10 }}>
+        Already have an account?{" "}
+        <Text style={{ color: "blue", fontWeight: "bold" }} onPress={() => {}}>
+          Login Now
+        </Text>
+      </Text>
+    </ScrollView>
+  );
+};
 
-export default Login
+export default Login;
 const styles = StyleSheet.create({
   inputLabel: {
-    color:COLORS.black,
+    color: COLORS.black,
   },
-})
+});
--- src/screens/auth/RegisterScreen.jsx
+++ src/screens/auth/RegisterScreen.jsx
@@ -1,12 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, StyleSheet, ScrollView } from "react-native";
+import React from "react";
+import { Button, Input, Text } from "@rneui/themed";
+import { COLORS } from "../../constants";
 
 const Register = () => {
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
-  )
-}
+    <ScrollView
+      style={{ width: "100%" }}
+      contentContainerStyle={{
+        flexGrow: 1,
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingHorizontal: 30,
+      }}
+    >
+      <Text h2>Login</Text>
+      <View
+        style={{ width: "100%", alignItems: "center", paddingVertical: 40 }}
+      >
+        <Input label={"Email"} labelStyle={styles.inputLabel} />
+        <Input label={"Password"} labelStyle={styles.inputLabel} />
+        <Button
+          color="primary"
+          title={"Login"}
+          size="lg"
+          containerStyle={{ width: 200, borderRadius: 5 }}
+        />
+      </View>
+      <Text style={{ paddingVertical: 10 }}>
+        Dont have an account?{" "}
+        <Text
+          style={{ color: "blue", fontWeight: "bold" }}
+          onPress={handleRegisterNowPress}
+        >
+          Register Now
+        </Text>
+      </Text>
+    </ScrollView>
+  );
+};
 
-export default Register
+export default Register;
+const styles = StyleSheet.create({
+  inputLabel: {
+    color: COLORS.black,
+  },
+});
