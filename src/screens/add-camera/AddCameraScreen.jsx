df33b0525da52f6a2361680671c7bb7ef66bf67b
Ibrahim <mibrahim37612@gmail.com>
2024-01-18 19:02:04 +0500

    Add styles and refactor AddCameraScreen.jsx and AddDeviceScreen.jsx

--- src/screens/add-camera/AddCameraScreen.jsx
+++ src/screens/add-camera/AddCameraScreen.jsx
@@ -1,4 +1,10 @@
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import React from "react";
 import { COLORS } from "../../constants";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
@@ -24,81 +30,27 @@ const AddCameraScreen = () => {
 
   const renderItem = ({ item, index }) => {
     return (
-      <View
-        style={{
-          width: "100%",
-          padding: 10,
-          height: 70,
-          marginTop: 10,
-          elevation: 1,
-          backgroundColor: COLORS.white,
-          alignItems: "center",
-          flexDirection: "row",
-        }}
-      >
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-            width: "75%",
-            paddingRight: 30,
-          }}
-        >
-          <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <View style={styles.itemContainer}>
+        <View style={styles.cameraInfoContainer}>
+          <View style={styles.cameraIconContainer}>
             <Icon size={32} color={COLORS.black} name="camera" />
-            <View style={{ marginLeft: 10 }}>
-              <Text style={{ fontWeight: 700 }}>{item.name}</Text>
+            <View style={styles.cameraInfo}>
+              <Text style={styles.cameraName}>{item.name}</Text>
               <Text>{item.ipAddress}</Text>
             </View>
           </View>
 
-          <View
-            style={{
-              backgroundColor: "#ECFDF5",
-              borderRadius: 20,
-              paddingHorizontal: 5,
-              justifyContent: "center",
-              paddingVertical: 2,
-              marginLeft: 30,
-            }}
-          >
-            <Text style={{ fontWeight: 500 }}>
+          <View style={styles.onlineIndicator}>
+            <Text style={styles.onlineIndicatorText}>
               {item.online ? "Online" : "Offline"}
             </Text>
           </View>
         </View>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "center",
-            width: "25%",
-          }}
-        >
-          <TouchableOpacity
-            style={{
-              width: 30,
-              height: 30,
-              backgroundColor: "#F7F8F9",
-              borderRadius: 100,
-              marginHorizontal: 5,
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
+        <View style={styles.actionsContainer}>
+          <TouchableOpacity style={styles.actionIconContainer}>
             <Icon size={26} color={COLORS.danger} name="delete" />
           </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              width: 30,
-              height: 30,
-              backgroundColor: "#F7F8F9",
-              borderRadius: 100,
-              marginHorizontal: 5,
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
+          <TouchableOpacity style={styles.actionIconContainer}>
             <Icon size={26} color={COLORS.black} name="plus" />
           </TouchableOpacity>
         </View>
@@ -106,11 +58,9 @@ const AddCameraScreen = () => {
     );
   };
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ paddingTop: 25, paddingHorizontal: 10 }}>
-        <Text style={{ fontSize: 24, fontWeight: 500 }}>
-          Discovered Cameras
-        </Text>
+    <View style={styles.container}>
+      <View style={styles.discoveredCameras}>
+        <Text style={styles.heading}>Discovered Cameras</Text>
         <FlatList data={cameras} renderItem={renderItem} />
       </View>
     </View>
@@ -118,3 +68,69 @@ const AddCameraScreen = () => {
 };
 
 export default AddCameraScreen;
+
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 24,
+    fontWeight: 500,
+  },
+  discoveredCameras: {
+    paddingTop: 25,
+    paddingHorizontal: 10,
+  },
+  container: {
+    flex: 1,
+  },
+  actionIconContainer: {
+    width: 30,
+    height: 30,
+    backgroundColor: "#F7F8F9",
+    borderRadius: 100,
+    marginHorizontal: 5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  actionsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "25%",
+  },
+  onlineIndicatorText: {
+    fontWeight: "500",
+  },
+  onlineIndicator: {
+    backgroundColor: "#ECFDF5",
+    borderRadius: 20,
+    paddingHorizontal: 5,
+    justifyContent: "center",
+    paddingVertical: 2,
+    marginLeft: 30,
+  },
+  cameraName: {
+    fontWeight: "700",
+  },
+  cameraInfo: {
+    marginLeft: 10,
+  },
+  cameraIconContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  cameraInfoContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "75%",
+    paddingRight: 30,
+  },
+  itemContainer: {
+    width: "100%",
+    padding: 10,
+    height: 70,
+    marginTop: 10,
+    elevation: 1,
+    backgroundColor: COLORS.white,
+    alignItems: "center",
+    flexDirection: "row",
+  },
+});
--- src/screens/add-device/AddDeviceScreen.jsx
+++ src/screens/add-device/AddDeviceScreen.jsx
@@ -15,6 +15,7 @@ import { check, request, PERMISSIONS, RESULTS } from "react-native-permissions";
 import { Button } from "@rneui/base";
 import { GlobalStyles } from "../../global/GlobalStyles";
 import { Text } from "@rneui/themed";
+import { COLORS } from "../../constants";
 
 const showCodeAlert = (value, onDismissed) => {
   const buttons = [
@@ -131,16 +132,34 @@ export default function CodeScannerPage({ navigation }) {
           />
         </View>
       ) : (
-        <>
+        <View style={GlobalStyles.container}>
+          <Text
+            style={{
+              color: COLORS.black,
+              fontSize: 18,
+              marginBottom: 30,
+              fontWeight: 700,
+            }}
+          >
+            Scan the QR code of your device.
+          </Text>
           {device != null && (
-            <Camera
-              style={StyleSheet.absoluteFill}
-              device={device}
-              isActive={isActive}
-              codeScanner={codeScanner}
-              torch={torch ? "on" : "off"}
-              enableZoomGesture={true}
-            />
+            <View
+              style={{
+                width: "100%",
+                flex: 0.6,
+                alignSelf: "center",
+              }}
+            >
+              <Camera
+                style={StyleSheet.absoluteFill}
+                device={device}
+                isActive={isActive}
+                codeScanner={codeScanner}
+                torch={torch ? "on" : "off"}
+                enableZoomGesture={true}
+              />
+            </View>
           )}
 
           <View style={styles.rightButtonRow}>
@@ -164,7 +183,7 @@ export default function CodeScannerPage({ navigation }) {
           >
             <IonIcon name="close-outline" color="white" size={35} />
           </PressableOpacity>
-        </>
+        </View>
       )}
     </View>
   );
