0b08ddeac20f84a872df39d96e27aa5fa4182804
Ibrahim <mibrahim37612@gmail.com>
2024-02-02 03:08:38 +0500

    Add button to start streaming

--- src/screens/live-stream/LiveStream.jsx
+++ src/screens/live-stream/LiveStream.jsx
@@ -1,43 +1,50 @@
 import React, { useState, useEffect, useRef } from "react";
-import { View, Image } from "react-native";
+import { View, Image, Button } from "react-native";
 import initializeSocket from "../../socket";
 import { decode as atob, encode as btoa } from "base-64";
-
+let socket = null;
 const LiveStream = () => {
-  const [buffer, setBuffer] = useState([]);
   const [currentFrame, setCurrentFrame] = useState(null);
+  let buffer = useRef([]);
   let intervalId = null;
   let framecount = useRef(0);
-  const startStreaming = async () => {
+
+  const initializeStreaming = async () => {
     try {
-      const socket = await initializeSocket();
-      socket.on("stream", ({ frame }) => {
-        console.log("frame", framecount.current++);
-        setBuffer((prevBuffer) => [...prevBuffer, frame]);
+      socket = await initializeSocket();
+      socket.on("stream:send", ({ frame }) => {
+        let base64 = btoa(
+          new Uint8Array(frame).reduce(
+            (data, byte) => data + String.fromCharCode(byte),
+            ""
+          )
+        );
+        base64 = atob(base64);
+        buffer.current.push(base64);
+        setCurrentFrame(base64);
+        // console.log("frame", framecount.current++);
       });
-      //   intervalId = setInterval(() => {
-      //     console.log(buffer.length);
-      //     if (buffer.length > 0) {
-      //       let frame = buffer.shift();
-      //       let base64 = btoa(
-      //         new Uint8Array(frame).reduce(
-      //           (data, byte) => data + String.fromCharCode(byte),
-      //           ""
-      //         )
-      //       );
-      //       base64 = atob(frame);
-      //       //   console.log("Frame", base64);
-      //       // Update the state to trigger a re-render with the new image
-      //       setCurrentFrame(base64);
-      //     }
-      //   }, 1000 / 30);
+      // intervalId = setInterval(() => {
+      //   if (buffer.current.length > 24) {
+      //     console.log("shifting buffer");
+      //     let frame = buffer.current.shift();
+      //     setCurrentFrame(frame);
+      //   }
+      // }, 1000 / 24);
     } catch (err) {
       console.log(err);
     }
   };
 
+  const startStreaming = async () => {
+    socket.emit("stream:start", {
+      deviceId: "abc",
+      cameraIP: "192.168.1.7:8554",
+    });
+  };
+
   useEffect(() => {
-    startStreaming();
+    initializeStreaming();
     return () => {
       if (intervalId) {
         clearInterval(intervalId);
@@ -46,12 +53,15 @@ const LiveStream = () => {
   }, []);
 
   return (
-    <Image
-      source={{
-        uri: `data:image/jpeg;base64,${currentFrame}`,
-      }}
-      style={{ width: 300, height: 200 }}
-    />
+    <View>
+      <Image
+        source={{
+          uri: `data:image/jpeg;base64,${currentFrame}`,
+        }}
+        style={{ width: "100%", height: 300 }}
+      />
+      <Button title="Start Streaming" onPress={startStreaming} />
+    </View>
   );
 };
 
